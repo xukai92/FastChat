7176ce6d3817af1cb4aba198e71b4a3d585fdddd
Kai Xu <xuk@ibm.com>
2024-04-16 04:13:00 +0000

    feat: update judge

--- fastchat/llm_judge/wiring-brute-force-judge.sh
+++ fastchat/llm_judge/wiring-brute-force-judge.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-for rank in {0..9}
+for cuda in {0..7}
     do
-    for cuda in {0..7}
+    for iter in {0..3}
         do
-        for iter in {0..3}
-            do
-            suffix="rank=$rank=cuda=$cuda=iter=$iter"
-            cfg_path="/root/notebooks/looped/cfgs/cfg-$suffix.yaml"
-            ans_path="/root/notebooks/looped/FastChat/fastchat/llm_judge/data/mt_bench/model_answer/merlinite-7b-$suffix.jsonl"
-            if [ -f $ans_path ]; then
-                /root/fsdp/bin/python gen_judgment.py \
-                    --bench-name mt_bench \
-                    --model-list merlinite-7b-$suffix \
-                    --parallel 40
-            fi
-        done
+        suffix="rank=$RANK=cuda=$cuda=iter=$iter"
+        cfg_path="/xuk/wiring/data/cfgs-$WORLD_SIZE/cfg-$suffix.yaml"
+        ans_path="data/mt_bench/model_answer/merlinite-7b-$suffix.jsonl"
+        if [ -f $ans_path ]; then
+            /root/fsdp/bin/python gen_judgment.py \
+                --bench-name mt_bench \
+                --model-list merlinite-7b-$suffix \
+                --parallel 40
+        fi
     done
 done
+cp data/mt_bench/model_judgement/gpt-4-single.jsonl \
+    /xuk/wiring/data/model_judgement/gpt-4-single-rank=$RANK.jsonl
